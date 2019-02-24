b144345a494177952dc8df6a82d43e83c1c8aba3
Anthony Ellis <AEllisNOLA@gmail.com>
2019-02-24 12:21:44 -0600

    Checkpoint 2759 works w/ small bug. Committing pre-fix. Bug = songs on album 1 continue to play even when songs on album 2 are played.

--- src/components/Album.js
+++ src/components/Album.js
@@ -10,10 +10,39 @@ class Album extends Component {
         });
 
         this.state = {
-            album: album
+            album: album,
+            currentSong: album.songs[0],
+            isPlaying: false
         };
+
+        this.audioElement = document.createElement('audio');
+        this.audioElement.src = album.songs[0].audioSrc;
     }
 
+    play() {
+        this.audioElement.play();
+        this.setState({ isPlaying: true })
+    }
+
+    pause() {
+        this.audioElement.pause();
+        this.setState({ isPlaying: false })
+    }
+
+    setSong(song) {
+        this.audioElement.src = song.audioSrc;
+        this.setState({ currentSong: song });
+    }
+
+    handleSongClick(song) {
+        const isSameSong = this.state.currentSong === song;
+        if (this.state.isPlaying && isSameSong) {
+            this.pause();
+        } else {
+            if (!isSameSong) {this.setSong(song);}
+            this.play();
+        }
+    }
     render() {
         return (
             <section className="album">
@@ -34,8 +63,8 @@ class Album extends Component {
                     <tbody>
                         {this.state.album.songs.map((song, index) => {
                             return ( 
-                            <tr key={index}>
-                                <td>Track {index + 1}</td>
+                            <tr className="song" key={index} onClick={() => this.handleSongClick(song)} >
+                                <td>{index + 1}</td>
                                 <td>{song.title}</td>
                                 <td>{song.duration}</td>
                             </tr>
