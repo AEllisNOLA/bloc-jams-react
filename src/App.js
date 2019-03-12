6114585e8ff10057fe7cf2bd56df3d1f8b391e37
Anthony <aellisnola@gmail.com>
2019-03-11 19:09:26 -0500

    Beginning styling. Navbar created

--- src/App.js
+++ src/App.js
@@ -1,30 +1,42 @@
-import React, { Component } from 'react';
-import { Route, Link } from 'react-router-dom';
-import './App.css';
-import Landing from './components/Landing';
-import Library from './components/Library';
-import Album from './components/Album';
+import React, { Component } from "react";
+import { Route, Link } from "react-router-dom";
+import "./App.css";
+import Landing from "./components/Landing";
+import Library from "./components/Library";
+import Album from "./components/Album";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header>
-          Bloc Jams
+        <header className="ui container">
+          <nav className="ui fixed inverted menu">
+            <Link className="item" to="/">
+              <img
+                style={{ "font-size": 30 }}
+                className="logo"
+                src={require("./logo/bloc_jams_logo.png")}
+                alt="Bloc Jams logo"
+              />
+            </Link>
+            <Link className="item" to="/library">
+              Library
+            </Link>
+            <Link className="item" to="/album">
+              Album
+            </Link>
+          </nav>
         </header>
 
-        <nav>
-          <Link to="/">Landing</Link>
-          <Link to="/library">Library</Link>
-          <Link to="/album">Album</Link>
-        </nav>
+        <header>Bloc Jams</header>
+
+        <nav />
 
         <main>
-          <Route exact path='/' component={Landing} />
+          <Route exact path="/" component={Landing} />
           <Route path="/library" component={Library} />
-          <Route path="/album/:slug" component={Album}></Route>
+          <Route path="/album/:slug" component={Album} />
         </main>
-        
       </div>
     );
   }
